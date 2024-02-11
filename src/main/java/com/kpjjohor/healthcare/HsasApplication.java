99788fd6e53d270518817a4fd3ce911fc00316f0
sitimahirahmtahrir <156703146+sitimahirahmtahrir@users.noreply.github.com>
2024-02-12 04:31:54 +0800

    Add files via upload

--- src/main/java/com/kpjjohor/healthcare/HsasApplication.java
+++ src/main/java/com/kpjjohor/healthcare/HsasApplication.java
@@ -2,12 +2,14 @@ package com.kpjjohor.healthcare;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.kpjjohor.healthcare.repository")
+@EntityScan(basePackages = "com.kpjjohor.healthcare.model")
 public class HsasApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(HsasApplication.class, args);
     }
-
 }
--- src/main/java/com/kpjjohor/healthcare/repository/AppointmentRepository.java
+++ src/main/java/com/kpjjohor/healthcare/repository/AppointmentRepository.java
@@ -2,7 +2,9 @@ package com.kpjjohor.healthcare.repository;
 
 import com.kpjjohor.healthcare.model.Appointment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
     // Add custom query methods if needed
 }
